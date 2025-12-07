eee5513bdee9212a26cecd6852b611b652716963
Alonso Cavalcante <231538001+acavalcante164-collab@users.noreply.github.com>
2025-12-07 13:41:34 -0400

    up

---
+++ BSequencial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int buscaSequencial(int *lista , int chave, int tamanho){
+    int i;
+    for(i = 0 ; i < tamanho ; i++){
+        if (lista[i] == chave){ //VAI DE POSICAO EM POSICAO PEGUNTANDO SE O INTEM ESTA NAQUELA POSICAO E  == A CHAVE
+            return i;
+        }
+    }
+    return -1;
+}
+void main (){
+    int numeros [] = {20 , 5 , 15 , 24 , 67 , 45 , 1 , 76 , 21 , 11};
+    int chave , resultado; //O ITEM BUSCADO E ARMAZENAMENTO DO RESULTADO
+
+    printf("Digite o numero a ser buscado: ");
+    scanf(" %d", &chave);
+
+    //FUNCAO DE BUSCA
+    resultado = buscaSequencial();
+
+    if (resultado != -1){
+        printf("O elemento %d foi encontrado no indice %d", chave, resultado);
+    }
+    else{
+        printf("O elemento %d nao foi encontrado.", &chave);
+    }
+}
